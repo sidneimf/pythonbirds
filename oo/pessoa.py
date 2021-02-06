b6cc50ab412300ba5ec8af37e550fb9fc244eeec
sidneimf <sidnei.mfrancisco@gmail.com>
2021-02-06 12:44:17 -0300

    Criada a Classe pessoa.py definindo teste parte 2 parte 3 Criados atributos nome e idade Campo complexo filhos Corrigido atribuição

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=18):
+    def __init__(self, *filhos, nome=None, idade=18):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Joao')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Sidnei'
-    print(p.nome)
-    print(p.idade)
+    joao = Pessoa(nome='Joao')
+    eliane = Pessoa(joao,nome='Eliane')
+    print(Pessoa.cumprimentar(eliane))
+    print(id(eliane))
+    print(eliane.cumprimentar())
+    print(eliane.nome)
+    print(eliane.idade)
+    for filho in eliane.filhos:
+        print(filho.nome)
