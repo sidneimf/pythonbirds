6c0c3555ddae5f55b1c9dc5a846c76f52ef5323e
sidneimf <sidnei.mfrancisco@gmail.com>
2021-02-07 18:17:38 -0300

    ajuste conforme padrões

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,6 +3,7 @@ from builtins import print, staticmethod
 
 class Pessoa:
     olhos = 2
+
     def __init__(self, nome=None, idade=18, *filhos, ):
         self.idade = idade
         self.nome = nome
@@ -19,34 +20,36 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+
 class Homem(Pessoa):
     pass
+
 
 class Mulher(Pessoa):
     pass
 
 
 if __name__ == '__main__':
-    joao = Pessoa('Joao',13)
-    eliane = Mulher('Eliane',44, joao)
-    michel = Pessoa('Michel',25)
-    dayane = Pessoa('Dayane',22)
-    sidnei = Homem('Sidnei',50, michel,dayane)
+    joao = Pessoa('Joao', 13)
+    eliane = Mulher('Eliane', 44, joao)
+    michel = Pessoa('Michel', 25)
+    dayane = Pessoa('Dayane', 22)
+    sidnei = Homem('Sidnei', 50, michel, dayane)
     print(Pessoa.cumprimentar(eliane))
     print(id(eliane))
     print(eliane.cumprimentar())
-    print(eliane.nome,eliane.idade)
+    print(eliane.nome, eliane.idade)
     for filho in eliane.filhos:
-        print('filho',filho.nome, filho.idade   )
+        print('filho', filho.nome, filho.idade)
     print(sidnei.nome, sidnei.idade)
     for filho in sidnei.filhos:
-        print('filho',filho.nome, filho.idade)
+        print('filho', filho.nome, filho.idade)
 
-    sidnei.sobrenome = 'Francisco' #atribuição dinamica, autilizar apenas em casos esporádicos
+    sidnei.sobrenome = 'Francisco'  # atribuição dinamica, autilizar apenas em casos esporádicos
     print(sidnei.__dict__)
     print(eliane.__dict__)
 
-    del sidnei.idade #não é uma boa pratica deletar atributos no código
+    del sidnei.idade  # não é uma boa pratica deletar atributos no código
     print(sidnei.__dict__)
     print(eliane.__dict__)
 
@@ -64,6 +67,9 @@ if __name__ == '__main__':
 
     print(Pessoa.metodo_estatico(), eliane.metodo_estatico())
 
-    print(joao.nome,'está na classe pessoa =',isinstance(joao, Pessoa),'está na classe Homem =',isinstance(joao, Homem),'está na classe Mulher =',isinstance(joao, Mulher))
-    print(eliane.nome,'está na classe pessoa =',isinstance(eliane, Pessoa),'está na classe Homem =',isinstance(eliane, Homem),'está na classe Mulher =',isinstance(eliane, Mulher))
-    print(sidnei.nome,'está na classe pessoa =',isinstance(sidnei, Pessoa),'está na classe Homem =',isinstance(sidnei, Homem),'está na classe Mulher =',isinstance(sidnei, Mulher))
+    print(joao.nome, 'está na classe pessoa =', isinstance(joao, Pessoa), 'está na classe Homem =',
+          isinstance(joao, Homem), 'está na classe Mulher =', isinstance(joao, Mulher))
+    print(eliane.nome, 'está na classe pessoa =', isinstance(eliane, Pessoa), 'está na classe Homem =',
+          isinstance(eliane, Homem), 'está na classe Mulher =', isinstance(eliane, Mulher))
+    print(sidnei.nome, 'está na classe pessoa =', isinstance(sidnei, Pessoa), 'está na classe Homem =',
+          isinstance(sidnei, Homem), 'está na classe Mulher =', isinstance(sidnei, Mulher))
